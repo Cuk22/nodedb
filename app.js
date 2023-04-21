5fe6cfae907766799528af1b032239be8df67655
Marko Cuk <marko.cuk.brains22@gmail.com>
2023-04-21 15:03:49 +0200

    4th commit

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const app = express();
 
 const dbURI = 'mongodb+srv://nekineki:test1234@klaster.ftrzt8x.mongodb.net/nodeDB?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then((result) => app.listen(3000) )
-.then((result) => console.log('connected to db'))
-.catch((err) => console.log(err));
+    .then((result) => app.listen(3000))
+    .then((result) => console.log('connected to db'))
+    .catch((err) => console.log(err));
 
 //register view engine
 //.set = configure app settings
@@ -20,11 +20,12 @@ app.set('view engine', 'ejs'); //ejs will be used in creating templates
 
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true })); //middleware for parsing data (takes url encoded data and parse it into object)
 app.use(morgan('dev')); //dev dictates how it will be formatted what we log in console
 
 app.get('/add-blog', (req, res) => {
     //new instance of blog model
-    const blog = new Blog ({
+    const blog = new Blog({
         title: 'new blog 2',
         snippet: 'about my new blog',
         body: 'more about my new blog'
@@ -41,22 +42,22 @@ app.get('/add-blog', (req, res) => {
 
 app.get('/all-blogs', (req, res) => {
     Blog.find()
-    .then((result) => {
-        res.send(result);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 });
 
 app.get('/single-blog', (req, res) => {
     Blog.findById('64426aa95f2d714c1253bf40')
-    .then((result) => {
-        res.send(result)
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 });
 
 //it will fire fn on every request because middleware is on top
@@ -77,7 +78,7 @@ app.get('/', (req, res) => { //2nd argument = fn takes two objects
     ];//dummy */
     //    res.send('<p>home page</p>'); //express auto do res.setHeader('Content-Type', 'text/html'); auto set status code
     //    res.sendFile('./views/index.html', { root: __dirname}); //2nd argument option object root of project
-// redirected to /blogs    res.render('index', { naslov: 'Home', blogs }); //knows as render view (take view, render it and send it to browser(app.set)), 2nd parametar data (object)
+    // redirected to /blogs    res.render('index', { naslov: 'Home', blogs }); //knows as render view (take view, render it and send it to browser(app.set)), 2nd parametar data (object)
 
 });
 
@@ -90,8 +91,21 @@ app.get('/about', (req, res) => { //2nd argument = fn takes two objects
 
 app.get('/blogs', (req, res) => {
     Blog.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('index', { naslov: 'All Blogs', blogs: result })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});
+
+app.post('/blogs', (req, res) => {
+    //console.log(req.body);
+    const blog = new Blog(req.body);
+
+    blog.save()
     .then((result) => {
-        res.render('index', { naslov: 'All Blogs', blogs: result})
+        res.redirect('/blogs');
     })
     .catch((err) => {
         console.log(err);
